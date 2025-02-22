50b6d5e51e8a1fcd2c3af6897b3baba66cf75a27
codrutalugoj <codrutalugoj@gmail.com>
2025-02-22 20:19:39 +0000

    add scraped info to json

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # LessWrong AI Alignment Trends
-End-to-end data engineering project where we extract LessWrong posts information,  
+End-to-end data engineering project where we extract LessWrong posts information, and visualize trends in AI Alignment posts.
 
 # Data Diagram/Workflow
 TODO
@@ -7,8 +7,8 @@ TODO
 # ETL Process
 
 ## Extract
-1. Scraping latest LessWrong posts
-- using requests & Beautiful Soup 
+1. Scraping latest LessWrong posts using requests & Beautiful Soup to get the raw html information
+2. Extract information from the HTML like title, author name, post tags and karma.  
 
 
 2. Storage
--- scraper.py
+++ scraper.py
@@ -1,62 +1,84 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import datetime
 import pandas as pd
+import json
 
-base_url = "https://www.lesswrong.com"
-#recent_posts_url = f"{base_url}/allPosts"
-#recent_posts_url = f"{base_url}/allPosts?after=2025-02-19&before=2025-02-20&limit=100"
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-page = requests.get(base_url, headers=headers)
-# .content contains the raw bytes, while .text contains decoded text in human-readable format
-soup = BeautifulSoup(page.content, "html.parser")
-
-
-post_items = soup.find_all("div", class_="LWPostsItem-row")
-posts_data = []
-
-
-
-for i, post_item in enumerate(post_items[:2]):
-    # Extract post URL
-    post_span = post_item.find("span", class_="PostsTitle-eaTitleDesktopEllipsis")
-    post_link = post_span.find("a")
-    if not post_link:
-        continue
+def save_to_json(data: dict, filename: str):
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"lesswrong_posts_{timestamp}.json"
     
-    post_url = f"{base_url}{post_link['href']}"
-    print(f"Scraping post {i+1}/20: {post_url}")
+    # Save to JSON file
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
     
-    # Get individual post content
-    post_page = requests.get(post_url, headers=headers)
-    post_soup = BeautifulSoup(post_page.content, "html.parser")
-    
-    # Extract post title
-    title = post_soup.find("h1", class_="PostsPageTitle-root")
-    title_text = title.find("a").text if title else "No title found"
-    print(f"    Post title: {title_text}")
+    print(f"Saved {len(data['posts'])} posts to {filename}")
+    return filename
 
-    # Extract tags
-    tags_root = post_soup.find("span", class_="FooterTagList-root")
-    tags_list = tags_root.find_all("span", class_="FooterTag-name")
-    tags = [tag.text for tag in tags_list]
-    print(f"    Tags: {tags}")
+def scrape_lw(num=10):
+    base_url = "https://www.lesswrong.com"
+    #recent_posts_url = f"{base_url}/allPosts"
+    #recent_posts_url = f"{base_url}/allPosts?after=2025-02-19&before=2025-02-20&limit=100"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    page = requests.get(base_url, headers=headers)
+    # .content contains the raw bytes, while .text contains decoded text in human-readable format
+    soup = BeautifulSoup(page.content, "html.parser")
 
-    # Extract author names
-    authors = post_soup.find("span", class_="PostsAuthors-authorName")
-    
-    author_names = [author.text for author in authors if author != ", "]
 
-    # Extract author links
-    authors_links = [author_res['href'] for author_res in authors.find_all("a", class_="UsersNameDisplay-noColor")]
+    post_items = soup.find_all("div", class_="LWPostsItem-row")
+    posts_data = {"timestamp": datetime.datetime.now().strftime("%Y%m%d_%H%M%S"),
+                  "posts": []}
 
-    # Extract upvotes/karma
-    karma = post_soup.find("h1", class_="Typography-root Typography-headline LWPostsPageTopHeaderVote-voteScore")
-    
-    print(f"    Author Names: {author_names}")
-    print(f"    Author links: {authors_links}")
-    print(f"    Post Karma: {karma.text}")
 
-    time.sleep(1)
+    for i, post_item in enumerate(post_items[:num]):
+        # Extract post URL
+        post_span = post_item.find("span", class_="PostsTitle-eaTitleDesktopEllipsis")
+        post_link = post_span.find("a")
+        if not post_link:
+            continue
+        
+        post_url = f"{base_url}{post_link['href']}"
+        print(f"Scraping post {i+1}/20: {post_url}")
+        
+        # Get individual post content
+        post_page = requests.get(post_url, headers=headers)
+        post_soup = BeautifulSoup(post_page.content, "html.parser")
+        
+        # Extract post title
+        title_raw = post_soup.find("h1", class_="PostsPageTitle-root")
+        title = title_raw.find("a").text if title_raw else "None"
+        print(f"    Post title: {title_raw}")
 
-print(f"Successfully scraped {len(posts_data)} posts")
+        # Extract tags
+        tags_root = post_soup.find("span", class_="FooterTagList-root")
+        tags_list = tags_root.find_all("span", class_="FooterTag-name")
+        tags = [tag.text for tag in tags_list]
+        print(f"    Tags: {tags}")
+
+        # Extract author names
+        authors = post_soup.find("span", class_="PostsAuthors-authorName")
+        
+        author_names = [author.text for author in authors if author != ", "]
+
+        # Extract author links
+        author_links = [author_res['href'] for author_res in authors.find_all("a", class_="UsersNameDisplay-noColor")]
+
+        # Extract upvotes/karma
+        karma = post_soup.find("h1", class_="Typography-root Typography-headline LWPostsPageTopHeaderVote-voteScore")
+        karma = karma.text if karma else "None"
+        
+        post_dict = {"title": title,
+                     "url": post_url,
+                     "author_names": author_names,
+                     "author_links": author_links,
+                     "tags": tags,
+                     "post_karma": karma}
+        
+        posts_data["posts"].append(post_dict)
+        time.sleep(2)
+
+    save_to_json(posts_data, filename="raw_data.json")
+
+if __name__ == "__main__":
+    scrape_lw(num=5)
