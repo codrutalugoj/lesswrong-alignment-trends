1e7361f21135e88a08b0dc6e31b61b7ae8d8ed5f
codrutalugoj <codrutalugoj@gmail.com>
2025-03-20 19:40:21 +0000

    add json to s3 script

---
+++ json_to_s3.py
@@ -0,0 +1,15 @@
+import json
+import boto3    
+
+def upload_json(json_filename: str):
+
+    s3 = boto3.client('s3')
+    json_object = json.load(json_filename)
+
+    s3.put_object(
+        Body=json.dumps(json_object),
+        Bucket='lw-trends',
+        Key=json_filename
+    )
+
+
