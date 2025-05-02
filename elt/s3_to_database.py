4bed3ca0f9e951f53a18c23221ff5719e8574862
codrutalugoj <codrutalugoj@gmail.com>
2025-05-02 15:35:11 +0100

    fixed connection error to postgres (ETL pipeline complete)

--- elt/s3_to_database.py
+++ elt/s3_to_database.py
@@ -11,19 +11,22 @@ def get_latest_s3_key(bucket) -> str:
 
 def json_to_database(filename: str):
     source_config = {
-    'dbname': 'source_db',
+    'dbname': 'destination_db',
     'user': 'postgres',
     'password': 'pwd',
-    'host': 'postgres'
+    'host': 'destination_postgres'
     }
     destination_filename = filename.split(sep=".")[0]
+
+    # expose the password for PG
+    subprocess_env = dict(PGPASSWORD=source_config['password'])
 
     result = subprocess.run(['pg_dump',
                             '-h', source_config['host'],
                             '-U', source_config['user'],
                             '-d', source_config['dbname'],
                             '-f', f'{destination_filename}.sql',
-                            '-w'], check=True)
+                            '-w'], env=subprocess_env, check=True)
 
 if __name__ == "__main__":
     filename = get_latest_s3_key(bucket="lw-trends")
