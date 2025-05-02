7862be9f6b910fcc8da2fb15165923c6e3855b2f
codrutalugoj <codrutalugoj@gmail.com>
2025-05-02 15:35:36 +0100

    add rest of dbt project files

---
+++ elt/transform.sql
@@ -0,0 +1,8 @@
+select 
+    post.title, 
+    post.author_names, 
+    post.tags as tags, 
+    post.post_karma as karma
+from trends 
+LATERAL VIEW EXPLODE(posts) as post
+WHERE NOT array_contains(post.tags, "Site Meta") 
