caa0c25772ed9ed0565f260404661613f1e836c9
codrutalugoj <codrutalugoj@gmail.com>
2025-05-13 14:37:52 +0100

    bugfix: select into not allowed in dbt

---
+++ lesswrong/models/transform/transform.sql
@@ -0,0 +1,8 @@
+SELECT
+  post.title as title,
+  post.author_names as author_names,
+  post.date as date,
+  post.tags as tags,
+  post.post_karma as karma
+FROM lesswrong_data as post
+WHERE NOT 'Site Meta' = post.tags
