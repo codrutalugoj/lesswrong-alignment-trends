945e5072f391e7c27a0753bf4e69198c604312c0
codrutalugoj <codrutalugoj@gmail.com>
2025-05-23 15:11:15 +0100

    initial bertopic topic modeling (does not work great)

--- modeling/topic_modeling.py
+++ modeling/topic_modeling.py
@@ -1,12 +1,21 @@
-# BertTopic using pretrained models
+# BertTopic using a pretrained model
 from bertopic import BERTopic
 import json
+import os
 
 topic_model = BERTopic.load("MaartenGr/BERTopic_Wikipedia")
 
-json_data = json.load("./../lesswrong_data.json")
-print(json_data)
-corpus = []
+with open(os.path.join(os.getcwd(), "lesswrong_data.json")) as f:
+    json_data = json.load(f)["posts"]
 
-# topic, prob = topic_model.transform("")
-# topic_model.topic_labels_[topic]
+corpus = []
+for post in json_data:
+    corpus.append(post["text"])
+
+topics, prob = topic_model.transform(corpus)
+
+for topic in topics:
+    print(topic_model.topic_labels_[topic])
+    # BERTopic identifies these topics:
+    # 1082_robots_robot_robotic_robotics
+    # 1212_caffeine_caffeinated_drowsiness_coffee
